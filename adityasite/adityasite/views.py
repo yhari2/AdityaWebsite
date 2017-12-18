eecb3cbeccf0710cdcfaf9166809f3c3017c8823
yhari2 <yhari@andrew.cmu.edu>
2017-12-18 15:49:34 -0500

    set up database
    need to determine materialize/react

--- adityasite/adityasite/views.py
+++ adityasite/adityasite/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.base import View
+from database.models import Post
 
 class HomePage(View):
     def get(self, request):
-        return render(request, 'index.html')
+        postsList = Post.objects.all()[::-1]
+        context = {}
+        context["posts"] = postsList
+        return render(request, 'index.html', context)
---
+++ adityasite/database/models.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    imgURL = models.CharField(max_length=400)
+    caption = models.TextField(default="")
+    postDate = models.DateField()
+   
+
+    
